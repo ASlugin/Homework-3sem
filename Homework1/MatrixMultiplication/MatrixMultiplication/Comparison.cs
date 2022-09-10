9520bc16a565a8b6e5f02e78b3db8d119f00d673
ASlugin <slugin.alexander@gmail.com>
2022-09-10 18:32:48 +0300

    Fixed

--- Homework1/MatrixMultiplication/MatrixMultiplication/Comparison.cs
+++ Homework1/MatrixMultiplication/MatrixMultiplication/Comparison.cs
@@ -1,6 +1,6 @@
-﻿using System.Diagnostics;
+﻿namespace MatrixMultiplication;
 
-namespace MatrixMultiplication;
+using System.Diagnostics;
 
 /// <summary>
 /// Class for comparing matrix multiplication methods

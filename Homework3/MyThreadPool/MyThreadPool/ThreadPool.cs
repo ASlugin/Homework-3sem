7909fb051b8c0bf5eb321e7fef92d1db38b086e6
ASlugin <slugin.alexander@gmail.com>
2022-11-23 22:31:04 +0300

    Initial commit

---
+++ Homework3/MyThreadPool/MyThreadPool/ThreadPool.cs
@@ -0,0 +1,16 @@
+﻿namespace MyThreadPool;
+
+public class MyThreadPool
+{
+    private Thread[] threads;
+
+    public MyThreadPool(int amountOfThreads)
+    {
+        if (amountOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Amount of threads must be positive");
+        }
+        threads = new Thread[amountOfThreads];
+    }
+
+}

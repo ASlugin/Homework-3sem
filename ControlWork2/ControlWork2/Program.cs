de8e6a03591da95a186316a63dc56080baa34a62
ASlugin <slugin.alexander@gmail.com>
2022-12-14 12:54:13 +0300

    Add chat

---
+++ ControlWork2/ControlWork2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ControlWork2;
+
+using System.Net;
+using System.Threading;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            var server = new Server(int.Parse(args[0]));
+            await server.Start();
+        }
+        if (args.Length == 2)
+        {
+            var client = new Client(IPAddress.Parse(args[0]), int.Parse(args[1]));
+            await client.Start();
+        }
+    }
+}

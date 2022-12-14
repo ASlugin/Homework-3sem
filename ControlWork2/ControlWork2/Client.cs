60e2ba1da3dd7e8b6e4fdd85cd5e8fadde22a18c
ASlugin <slugin.alexander@gmail.com>
2022-12-14 12:40:00 +0300

    Add chat

---
+++ ControlWork2/ControlWork2/Client.cs
@@ -0,0 +1,54 @@
+﻿namespace ControlWork2;
+
+using System.Net;
+using System.Net.Sockets;
+
+public class Client
+{
+    private IPAddress ip;
+    private int port;
+
+    public Client(IPAddress ip, int port)
+    {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public async Task Start()
+    {
+        using (var client = new TcpClient())
+        {
+            client.Connect(ip, port);
+            var stream = client.GetStream();
+            Console.WriteLine("Соединение установлено");
+
+            var writer = new StreamWriter(stream);
+            var reader = new StreamReader(stream);
+
+            while (true)
+            {
+                /*
+                await Task.Run(async () =>
+                {
+                    var reader = new StreamReader(stream);
+
+                    var data = await reader.ReadLineAsync();
+                    Console.WriteLine(data);
+                });*/
+
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        var message = Console.ReadLine();
+                        await writer.WriteLineAsync(message);
+                        await writer.FlushAsync();
+                    }
+                });
+            }
+        }
+
+        Console.WriteLine("Client stopped");
+    }
+
+}
---
+++ ControlWork2/ControlWork2/Server.cs
@@ -0,0 +1,56 @@
+﻿namespace ControlWork2;
+
+using System.Net;
+using System.Net.Sockets;
+
+public class Server
+{
+    private int port;
+    
+    public Server (int port)
+    {
+        this.port = port;
+    }
+
+    public async Task Start()
+    {
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Server started on port {port}...");
+        
+        var socket = await listener.AcceptSocketAsync();
+        Console.WriteLine("Соединение с клиентом установлено");
+
+        var stream = new NetworkStream(socket);
+        var reader = new StreamReader(stream);
+        var writer = new StreamWriter(stream);
+        while (true)
+        { 
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var data = await reader.ReadLineAsync();
+                    if (data != null)
+                    {
+                        Console.WriteLine(data);
+                    }
+                }
+            });
+
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                }
+            });
+        }
+        socket.Close();
+        Console.WriteLine("Соединение с клиентом прервано");
+
+    }
+
+}

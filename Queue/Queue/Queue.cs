d9b2a08e024c617131a5fed17041752d8f108249
ASlugin <slugin.alexander@gmail.com>
2022-12-21 12:46:55 +0300

    Add queue with priorities

---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,59 @@
+﻿namespace Queue;
+
+/// <summary>
+/// Class for queue with priorities
+/// </summary>
+public class QueueWithPriority<T>
+{
+    private List<(T value, int priority)> list;
+
+    /// <summary>
+    /// Amount of elements in queue
+    /// </summary>
+    public int Size { private set; get; }
+
+    public QueueWithPriority()
+    {
+        this.list = new();
+        this.Size = 0;
+    }
+
+    /// <summary>
+    /// Adds element to queue according to priority
+    /// </summary>
+    public void Enqueue(T value, int priority)
+    {
+        lock (list)
+        {
+            var index = 0;
+            var length = list.Count;
+            while (index < length && priority <= list[index].priority)
+            {
+                index++;
+            }
+            list.Insert(index, (value, priority));
+            Size++;
+            Monitor.PulseAll(list);
+        }
+    }
+
+    /// <summary>
+    /// Returns and removes element from queue with the highest priority
+    /// </summary>
+    public T Dequeue()
+    {
+        T result;
+        lock (list)
+        {
+            while (Size == 0)
+            {
+                Monitor.Wait(list);
+            }
+
+            result = list[0].value;
+            list.RemoveAt(0);
+            Size--;
+        }
+        return result;
+    }
+}
---
+++ Queue/QueueTests/QueueTests.cs
@@ -0,0 +1,52 @@
+namespace QueueTests;
+using Queue;
+using System.Collections.Generic;
+
+public class Tests
+{
+    [Test]
+    public void QueueWithPriorityShallWorkCorrectlyWhenOneThread()
+    {
+        var queue = new QueueWithPriority<int>();
+
+        for (int i = 0; i < 11; ++i)
+        {
+            queue.Enqueue(i, i);
+        }
+
+        for (int i = 10; i >= 0; --i)
+        {
+            Assert.That(queue.Dequeue(), Is.EqualTo(i));
+        }
+    }
+
+    [Test]
+    public void QueueWithPriorityShallWorkCorrectlyWhenMultiThread()
+    {
+        var queue = new QueueWithPriority<int>();
+
+        var threads = new Thread[Environment.ProcessorCount];
+        for (int i = 0; i < threads.Length; ++i)
+        {
+            var localI = i;
+            threads[i] = new Thread(() =>
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    queue.Enqueue(j, j);
+                }
+            });
+        }
+        
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        Assert.That(queue.Size, Is.EqualTo(Environment.ProcessorCount * 10));
+    }
+}
